cd9cd1503ebba38a1b1c677d12273aa470ad2984
Ian Behrmann <ianjbehrmann@gmail.com>
2015-11-13 11:38:09 -0700

    post route adds memory

--- js/controllers.js
+++ js/controllers.js
@@ -4,4 +4,22 @@ app.controller('MemController', function ($scope, $http){
     console.log(response.data.data)
     $scope.memories = response.data.data;
   })
+  $scope.submit = function (){
+    var res = {
+        "type": "memory",
+        "attributes": {
+          "old_days": $scope.old_days,
+          "these_days": $scope.these_days,
+          "year": $scope.year
+        }
+    }
+    $http.post('http://g12-ian-behrmann-memories.cfapps.io/api/v1/memories', {data: res}).then(function(response){
+      console.log(response.data)
+    }, function(){
+        console.log('error');
+    });
+  $scope.old_days='';
+  $scope.these_days='';
+  $scope.year='';
+  }
 })
