5530d651e149fc5c59bf19253b3596f730429640
Ian Behrmann <ianjbehrmann@gmail.com>
2015-11-13 15:04:27 -0700

    http request successful to group service registry

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,8 @@
 app.controller('MemController', function ($scope, $http, $routeParams){
   $scope.title = "Angular Memories"
-  $http.get('http://g12-ian-behrmann-memories.cfapps.io/api/v1/memories').then(function(response){
-    $scope.memories = response.data.data;
-  })
+  // $http.get('http://g12-ian-behrmann-memories.cfapps.io/api/v1/memories').then(function(response){
+  //   $scope.memories = response.data.data;
+  // })
   $scope.submit = function (){
     var res = {
         "type": "memory",
@@ -24,8 +24,17 @@ app.controller('MemController', function ($scope, $http, $routeParams){
     $scope.memoryYears = response.data.data
   })
   $http.get('http://g12-ian-behrmann-memories.cfapps.io/api/v1/memories/' + $routeParams.year).then(function(response){
-    console.log(response.data.data)
     $scope.yearData = response.data.data
     $scope.year = $routeParams.year;
+  })
+  $http.get('http://galvanize-service-registry.cfapps.io/api/v1/cohorts/g12').then(function(response){
+    console.log(response, "RESPONSE")
+    var url = response.data.data[0].attributes.url
+    console.log(url)
+
+    $http.get(url).then(function(response){
+      console.log(response, "URL RESPONSE")
+      $scope.memories = response.data.data;
+    })
   })
 })
